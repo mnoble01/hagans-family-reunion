7d4537476b67d4ae889d68da75a34d8011720bc3
makala <makala.noble@gmail.com>
2018-08-05 19:46:17 -0400

    More content; dynamic planning committee page

--- app/pods/2019-reunion/committee/route.js
+++ app/pods/2019-reunion/committee/route.js
@@ -1,5 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import AirtableModel from 'hagans-family/pods/airtable/model';
 
 export default Route.extend({
-  titleToken: 'Planning Committee',
+  titleToken: 'People',
+
+  ajax: service(),
+
+  async model() {
+    const response = await this.get('ajax').request('/api/users');
+    const airtableUsers = response.map(u => new AirtableModel(u));
+    const users = airtableUsers.filter((user) => {
+      const groups = user.userGroups || [];
+      return groups.includes('2019 planning committee');
+    });
+    return { users };
+  },
 });
