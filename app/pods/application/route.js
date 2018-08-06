8bac3956f6602be703479d9ba60785ef8cc07902
makala noble <mnoble@salsify.com>
2018-08-06 13:58:09 -0400

    Rm airtable key from client code

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  airtable: service(),
   session: service(),
   router: service(),
 
