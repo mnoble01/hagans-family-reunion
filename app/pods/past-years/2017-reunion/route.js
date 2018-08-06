a64f10d1d94683349a8447d83578957c1ce297d8
makala <makala.noble@gmail.com>
2018-08-05 21:56:36 -0400

    Add a TODO

--- app/pods/past-years/2017-reunion/route.js
+++ app/pods/past-years/2017-reunion/route.js
@@ -2,4 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   titleToken: '2017',
+
+  // TODO make a route to extend from for past year routes
+  // and show all posts on index pages from past years
 });
