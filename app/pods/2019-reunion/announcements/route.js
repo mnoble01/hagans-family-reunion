8d6724d8e309c77d00feaec26b395b14e3db716a
makala <makala.noble@gmail.com>
2018-08-05 21:55:06 -0400

    Show 2019 announcements

--- app/pods/2019-reunion/announcements/route.js
+++ app/pods/2019-reunion/announcements/route.js
@@ -1,5 +1,26 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import AirtableModel from 'hagans-family/pods/airtable/model';
 
 export default Route.extend({
+  ajax: service(),
+
   titleToken: 'Announcements',
+
+  async model() {
+    const response = await this.get('ajax').request('/api/posts', {
+      data: {
+        status: 'Published',
+      },
+    });
+    const posts = response.map(post => new AirtableModel(post)).filter((post) => {
+      return (post.categories || []).includes('2019 Reunion');
+    });
+    const authorIds = posts.map(post => post.author).uniq();
+    const authorsResponses = await Promise.all(authorIds.map((authorId) => {
+      return this.get('ajax').request(`/api/users/${authorId}`);
+    }));
+    const authors = authorsResponses.map(author => new AirtableModel(author));
+    return { posts, authors };
+  },
 });
