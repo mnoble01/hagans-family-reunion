375c23f68f0f5e86b035c64f93ea1f81a4665252
makala noble <mnoble@salsify.com>
2018-08-08 12:31:43 -0400

    Tweaking

--- server/routes.js
+++ server/routes.js
@@ -297,10 +297,13 @@ function requireHttps(req, res, next) {
 //   }
 
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
-    return res.redirect('https://' + req.get('host') + req.url);
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development' && !req.url.startsWith('/api')) {
+    console.log('DO redirect', req.secure, req.host, req.url);
+    return res.redirect('https://' + req.host + req.url);
+  } else {
+    console.log('no redirect');
+    return next();
   }
-  next();
 }
 
 // redirect www to non-www
