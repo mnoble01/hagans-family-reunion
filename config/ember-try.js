d55b5c599a65a4a18e0471bc9de187a869dba565
makala noble <mnoble@salsify.com>
2018-08-09 11:38:10 -0400

    Add ember-try config

---
+++ config/ember-try.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = function() {
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary'),
+  ]).then((urls) => {
+    return {
+      scenarios: [
+        {
+          name: 'ember-release',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[0],
+            },
+          },
+        },
+        {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[1],
+            },
+          },
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[2],
+            },
+          },
+        },
+        {
+          name: 'ember-default',
+          npm: {
+            devDependencies: {},
+          },
+        },
+      ],
+    };
+  });
+};
