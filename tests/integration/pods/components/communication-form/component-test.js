730ec4116017ccfb376a6f3a4371e9cf7e43a469
makala noble <mnoble@salsify.com>
2019-02-01 22:38:25 -0500

    Rm failing test

--- tests/integration/pods/components/communication-form/component-test.js
+++ tests/integration/pods/components/communication-form/component-test.js
@@ -1,26 +1,6 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | communication-form', function(hooks) {
   setupRenderingTest(hooks);
-
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{communication-form}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#communication-form}}
-        template block text
-      {{/communication-form}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
 });
