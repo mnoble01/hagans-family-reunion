5cb970e11355e7648274a18f824141baec575bfd
makala noble <mnoble@salsify.com>
2019-02-02 22:01:23 -0500

    Rm route test

--- tests/unit/pods/reunion-registration/route-test.js
+++ tests/unit/pods/reunion-registration/route-test.js
@@ -1,11 +1,12 @@
-import { module, test } from 'qunit';
+// import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | 2019-registration', function(hooks) {
+module('Unit | Route | reunion-registration', function(hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
-    const route = this.owner.lookup('route:reunion-registration');
-    assert.ok(route);
-  });
+  // test('it exists', function(assert) {
+  //   const route = this.owner.lookup('route:reunion-registration');
+  //   assert.ok(route);
+  // });
 });
