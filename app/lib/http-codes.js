500bb881e415a2cdaccd745ae9263068b8a6cb3d
makala noble <mnoble@salsify.com>
2019-02-15 16:25:49 -0500

    Add ember route error handling

---
+++ app/lib/http-codes.js
@@ -0,0 +1,15 @@
+// 2xx Success
+export const OK = 200;
+export const CREATED = 201;
+export const NO_CONTENT = 204;
+
+// 4xx Client Error
+export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
+export const UNPROCESSABLE_ENTITY = 422;
+
+// 5xx Server Error
+export const INTERNAL_SERVER_ERROR = 500;
+export const SERVICE_UNAVAILABLE = 503;
